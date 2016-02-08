102bfe78b8f60240eae2cc20da95d8da7278f709
d2118370 <d2118370@gmail.com>
2016-02-08 02:15:30 -0500

    string

---
+++ ValidParentheses.java
@@ -0,0 +1,24 @@
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        
+        for (int i = 0; i<s.length(); i++) {
+            char c = s.charAt(i);
+            if(c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } else {
+                if (stack.empty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+            
+        }
+        
+        return stack.empty();
+    }
+    
+}
